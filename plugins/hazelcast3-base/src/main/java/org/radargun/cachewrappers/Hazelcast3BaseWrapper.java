2fd6ccf8fa5c3f65b7b6e7ffc7a4fc94810232bf
Donnerbart <github@thunderphreak.de>
2014-11-20 11:18:21 +0100

    Add base module for Hazelcast 3.x to provide single implementation for CacheWrapper.

---
+++ plugins/hazelcast3-base/src/main/java/org/radargun/cachewrappers/Hazelcast3BaseWrapper.java
@@ -0,0 +1,191 @@
+package org.radargun.cachewrappers;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import com.hazelcast.transaction.TransactionContext;
+import org.radargun.CacheWrapper;
+import org.radargun.features.AtomicOperationsCapable;
+import org.radargun.logging.Log;
+import org.radargun.logging.LogFactory;
+import org.radargun.utils.TypedProperties;
+
+import java.io.InputStream;
+
+/**
+ * An implementation of CacheWrapper that uses Hazelcast instance as an underlying implementation.
+ */
+public class Hazelcast3BaseWrapper implements CacheWrapper, AtomicOperationsCapable {
+
+   private static final String DEFAULT_MAP_NAME = "default";
+
+   private final Log log = LogFactory.getLog(getClass());
+   private final boolean trace = log.isTraceEnabled();
+
+   private HazelcastInstance hazelcastInstance;
+   private IMap<Object, Object> hazelcastMap;
+   private ThreadLocal<TransactionContext> transactionContext = new ThreadLocal<TransactionContext>();
+
+   /**
+    * CacheWrapper
+    */
+
+   @Override
+   public void setUp(String config, boolean isLocal, int nodeIndex, TypedProperties confAttributes) throws Exception {
+      log.info("Creating cache with the following configuration: " + config);
+
+      InputStream configStream = getAsInputStreamFromClassLoader(config);
+      Config cfg = new XmlConfigBuilder(configStream).build();
+      hazelcastInstance = Hazelcast.newHazelcastInstance(cfg);
+      log.info("Hazelcast configuration:" + hazelcastInstance.getConfig().toString());
+
+      String mapName = getMapName(confAttributes);
+      hazelcastMap = hazelcastInstance.getMap(mapName);
+   }
+
+   @Override
+   public void tearDown() throws Exception {
+      hazelcastInstance.getLifecycleService().shutdown();
+   }
+
+   @Override
+   public boolean isRunning() {
+      return hazelcastInstance.getLifecycleService().isRunning();
+   }
+
+   @Override
+   public int getNumMembers() {
+      if (trace) log.trace("Cluster size=" + hazelcastInstance.getCluster().getMembers().size());
+      if (!hazelcastInstance.getLifecycleService().isRunning())
+         return -1;
+      else
+         return hazelcastInstance.getCluster().getMembers().size();
+   }
+
+   @Override
+   public String getInfo() {
+      return "There are " + hazelcastMap.size() + " entries in the cache.";
+   }
+
+   @Override
+   public boolean isTransactional(String bucket) {
+      return true;
+   }
+
+   @Override
+   public void startTransaction() {
+      try {
+         TransactionContext newTransactionContext = hazelcastInstance.newTransactionContext();
+         transactionContext.set(newTransactionContext);
+         newTransactionContext.beginTransaction();
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   @Override
+   public void endTransaction(boolean successful) {
+      try {
+         TransactionContext tc = transactionContext.get();
+         transactionContext.remove();
+         if (successful) {
+            tc.commitTransaction();
+         } else {
+            tc.rollbackTransaction();
+         }
+      } catch (Exception e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   @Override
+   public int getLocalSize() {
+      return -1; //not supported by Hazelcast, local size can be monitored through Hazelcast management center (web GUI)
+   }
+
+   @Override
+   public int getTotalSize() {
+      return hazelcastMap.size();
+   }
+
+   /**
+    * BasicOperations
+    */
+
+   @Override
+   public void put(String bucket, Object key, Object value) throws Exception {
+      if (trace) log.trace("PUT key=" + key);
+      hazelcastMap.set(key, value);
+   }
+
+   @Override
+   public Object get(String bucket, Object key) throws Exception {
+      if (trace) log.trace("GET key=" + key);
+      return hazelcastMap.get(key);
+   }
+
+   @Override
+   public Object getReplicatedData(String bucket, String key) throws Exception {
+      return get(bucket, key);
+   }
+
+   @Override
+   public Object remove(String bucket, Object key) throws Exception {
+      if (trace) log.trace("REMOVE key=" + key);
+      return hazelcastMap.remove(key);
+   }
+
+   @Override
+   public void clear(boolean local) throws Exception {
+      if (local) {
+         log.warn("This cache cannot remove only local entries");
+      }
+      hazelcastMap.clear();
+   }
+
+   /**
+    * AtomicOperationsCapable
+    */
+
+   @Override
+   public boolean replace(String bucket, Object key, Object oldValue, Object newValue) throws Exception {
+      return hazelcastMap.replace(key, oldValue, newValue);
+   }
+
+   @Override
+   public Object putIfAbsent(String bucket, Object key, Object value) throws Exception {
+      return hazelcastMap.putIfAbsent(key, value);
+   }
+
+   @Override
+   public boolean remove(String bucket, Object key, Object oldValue) throws Exception {
+      return hazelcastMap.remove(key, oldValue);
+   }
+
+   private String getMapName(TypedProperties confAttributes) {
+      return confAttributes.containsKey("map") ? confAttributes.getProperty("map") : DEFAULT_MAP_NAME;
+   }
+
+   private InputStream getAsInputStreamFromClassLoader(String filename) {
+      ClassLoader cl = Thread.currentThread().getContextClassLoader();
+      InputStream is;
+      try {
+         is = cl == null ? null : cl.getResourceAsStream(filename);
+      } catch (RuntimeException re) {
+         // could be valid; see ISPN-827
+         is = null;
+      }
+      if (is == null) {
+         try {
+            // check system class loader
+            is = getClass().getClassLoader().getResourceAsStream(filename);
+         } catch (RuntimeException re) {
+            // could be valid; see ISPN-827
+            is = null;
+         }
+      }
+      return is;
+   }
+}
---
+++ plugins/hazelcast30/src/main/java/org/radargun/cachewrappers/Hazelcast3ExtendedBaseWrapper.java
@@ -0,0 +1,7 @@
+package org.radargun.cachewrappers;
+
+/**
+ * An implementation of CacheWrapper that uses Hazelcast instance as an underlying implementation.
+ */
+public class Hazelcast3ExtendedBaseWrapper extends Hazelcast3BaseWrapper {
+}
