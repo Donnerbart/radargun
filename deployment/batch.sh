a71316945180c9c107f56a26a469c4d60aba55ee
Donnerbart <github@thunderphreak.de>
2014-12-01 10:38:14 +0100

    Moved batch_download_reports.sh to batch.sh. Moved common parameters to variable in all batch scripts. Added batch load-key-number-and-ratio.

--- deployment/batch.sh
+++ deployment/batch.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # file to execute
-BATCH_FILE=example
+BATCH_FILE=''
 
 # path to the deployment folder
 BATCH_RADARGUN_DEPLOYMENT_DIR=./radargun/deployment
 
+BATCH_SOURCE_DIR=/tmp/reports/batch
+BATCH_TARGET_DIR=./reports/batch
+
 BATCH_USER=$(whoami)
 BATCH_HOST='192.168.2.101'
+
+DOWNLOAD_RESULTS=false
+RUN_LOCAL=false
 
 # override settings with local-settings file
 if [ -f local-settings ]; then
@@ -25,6 +31,20 @@ do
         "--host")
             BATCH_HOST="$2"
             shift
+            ;;
+        "--download")
+            DOWNLOAD_RESULTS=true
+            ;;
+        "--source")
+            BATCH_SOURCE_DIR="$2"
+            shift
+            ;;
+        "--target")
+            BATCH_TARGET_DIR="$2"
+            shift
+            ;;
+        "--local")
+            RUN_LOCAL=true
             ;;
         "--batch-file")
             BATCH_FILE="$2"
@@ -71,7 +91,35 @@ function port {
 ADDRESS=$(address ${BATCH_HOST})
 PORT=$(port ${BATCH_HOST})
 
-if ! [ -e tests/${BATCH_FILE} ]; then
+if [ "${DOWNLOAD_RESULTS}" = true ]; then
+    BATCH_TARGET_DIR=$(readlink -mv ${BATCH_TARGET_DIR})
+
+    mkdir -p ${BATCH_TARGET_DIR}
+    cd ${BATCH_TARGET_DIR}
+
+    if [ -f "batch.zip" ]; then
+        rm batch.zip
+    fi
+
+    echo Zipping batch reports...
+    ssh -a ${BATCH_USER}@${ADDRESS} -p ${PORT} "cd ${BATCH_SOURCE_DIR}; zip -r batch.zip ."
+
+    echo Downloading batch reports...
+    scp -C -P ${PORT} -r ${DOWNLOAD_USER}@${ADDRESS}:${BATCH_SOURCE_DIR}/batch.zip batch.zip
+
+    if [ -f "batch.zip" ]; then
+        echo Unpacking batch reports...
+        unzip batch.zip
+        echo Done!
+
+        exit 0
+    else
+        echo Could not download batch reports!
+        exit 1
+    fi
+fi
+
+if ! [ -f tests/${BATCH_FILE} ]; then
     echo "Can't find batch file: tests/${BATCH_FILE}"
     exit 1
 fi
@@ -79,12 +127,22 @@ fi
 cat benchmark-batch/batch-header.sh > benchmark-batch/batch-tmp.sh
 cat tests/${BATCH_FILE} >> benchmark-batch/batch-tmp.sh
 
-echo Copying batch file to remote server...
-scp -q -C -P ${PORT} benchmark-batch/batch-tmp.sh ${BATCH_USER}@${ADDRESS}:${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh &>/dev/null
-rm -rf benchmark-batch/batch-tmp.sh
+if [ "${RUN_LOCAL}" = true ]; then
+    echo Copying batch file locally...
+    cp benchmark-batch/batch-tmp.sh benchmark-batch/batch-execute.sh
+    rm -rf benchmark-batch/batch-tmp.sh
 
-echo Starting remote batch process...
-ssh -a ${BATCH_USER}@${ADDRESS} -p ${PORT} "chmod +x ${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh"
-ssh -a ${BATCH_USER}@${ADDRESS} -p ${PORT} "screen -d -m ${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh"
+    echo Starting local batch process...
+    chmod +x benchmark-batch/batch-execute.sh
+    ./benchmark-batch/batch-execute.sh
+else
+    echo Copying batch file to remote server...
+    scp -q -C -P ${PORT} benchmark-batch/batch-tmp.sh ${BATCH_USER}@${ADDRESS}:${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh &>/dev/null
+    rm -rf benchmark-batch/batch-tmp.sh
+
+    echo Starting remote batch process...
+    ssh -a ${BATCH_USER}@${ADDRESS} -p ${PORT} "chmod +x ${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh"
+    ssh -a ${BATCH_USER}@${ADDRESS} -p ${PORT} "screen -d -m ${BATCH_RADARGUN_DEPLOYMENT_DIR}/benchmark-batch/batch-execute.sh"
+fi
 
 echo Done!
